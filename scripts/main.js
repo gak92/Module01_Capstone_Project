a3921a5188c33532a0c4b2cef733bf3b0605444c
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-23 00:27:23 +0500

    Add functionality for More button on mobile version, and displaying the speakers boxes when resizing window from mobile to desktop and vice versa

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,50 @@
-// Javascript code here
+// Displaying speakers boxes on mobile when click on more button
+
+const btnMore = document.querySelector('#btn-more');
+const speakerBoxes = document.querySelectorAll('.speaker-box');
+
+btnMore.addEventListener('click', () => {
+
+    for(i=2; i<speakerBoxes.length; i+=1) {
+        const box = document.getElementById(speakerBoxes[i].id);
+
+        if (box.style.display === '' || box.style.display === 'none') {
+            box.style.display = 'flex';
+            btnMore.textContent = 'Less';
+        }
+        else if (box.style.display === 'flex') {
+            box.style.display = 'none';
+            btnMore.textContent = 'More';
+        }
+    }
+});
+
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+function isMediaChangedToDesktop(e) {
+  if (e.matches) {
+    console.log('Media Query Matched! min-width: 768px');
+    speakerBoxes.forEach((speakerBox) => {
+        const box = document.getElementById(speakerBox.id);
+        box.style.display = 'flex';
+    });
+  }
+}
+
+mediaQuery.addListener(isMediaChangedToDesktop);
+isMediaChangedToDesktop(mediaQuery);
+
+const mediaQuery2 = window.matchMedia('(max-width: 767px)');
+
+function isMediaChangedToMobile(e) {
+    if (e.matches) {
+        btnMore.textContent = 'More';
+        for (i=2; i<speakerBoxes.length; i+=1) {
+            const box = document.getElementById(speakerBoxes[i].id);
+            box.style.display = 'none';
+        }
+    }
+}
+
+mediaQuery2.addListener(isMediaChangedToMobile);
+isMediaChangedToMobile(mediaQuery2);
