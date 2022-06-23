669bd52201d8f2efdd4664c249e0182913b935de
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-23 13:40:46 +0500

    Add box shadow to the main menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,3 +1,4 @@
+// Mobile Menu Display
 const hamburger = document.querySelector('#hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
@@ -8,3 +9,20 @@ hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
     console.log(mobileMenu);
 });
+
+
+// Make the main menu bar fixed when scrolling
+const navBar = document.getElementById('navbar');
+let sticky = navBar.offsetTop;
+
+window.onscroll = function() {
+    stickyNavBar();
+};
+
+function stickyNavBar() {
+    if (window.pageYOffset >= sticky) {
+        navBar.classList.add("sticky")
+    } else {
+        navBar.classList.remove("sticky");
+    }
+}
